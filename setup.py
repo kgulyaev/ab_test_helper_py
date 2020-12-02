2e4db8095228ee4eab8bf6c38d1643fa3205e9ae
Kirill Gulyaev <k.gulyaev@municorn.com>
2020-12-02 20:51:57 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "ab_test_helper",
+    version = "0.0.1",
+    author = "Kirill Guliaev",
+    author_email = "k.gulyaev@municorn.com",
+    description = "",
+    license = "Apache 2",
+    keywords = "",
+    url = "https://github.com/kgulyaev/ab_test_helper_py",
+    packages=['ab_test_helper'],
+
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
