9e458ebfdfed63c01433122495e09dd548d0f6dc
Kirill Gulyaev <k.gulyaev@municorn.com>
2020-12-03 18:19:57 +0300

    return only p-value

--- ab_test_helper/ab_criteria.py
+++ ab_test_helper/ab_criteria.py
@@ -10,25 +10,25 @@ __author__ = 'Kirill Guliaev'
 
 def t_student(data1, data2):
 	t_statistic, p_value = ttest_ind(data1, data2)
-	return t_statistic, p_value
+	return p_value
 
 def z_proportions(successes_1, trials_1, successes_2, trials_2, h1='two-sided'):
 	z_statistic, p_value = proportions_ztest(
 		count=np.array([successes_1, successes_2]),
     	nobs=np.array([trials_1, trials_2]),
         alternative=h1)
-	return z_statistic, p_value
+	return p_value
 
 def chi_squared(successes_1, trials_1, successes_2, trials_2, yates_correction=True):
 	table = [[successes_1, successes_2], [trials_1, trials_2]]
 	chi2_statistic, p_value, df, expected = chi2_contingency(table, correction=yates_correction)
-	return chi2_statistic, p_value
+	return p_value
 
 def fisher(successes_1, trials_1, successes_2, trials_2):
 	table = [[successes_1, successes_2], [trials_1, trials_2]]
 	oddsratio, p_value = fisher_exact(table)
-	return oddsratio, p_value
+	return p_value
 
 def mann_whitneyu(data1, data2):
 	u_stat, p_value = mannwhitneyu(data1, data2)
-	return u_stat, p_value
+	return p_value
