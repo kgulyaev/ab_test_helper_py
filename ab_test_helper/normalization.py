8f8f48d9769ee4035c4ac4c9cc29deb9d72708d1
Kirill Gulyaev <k.gulyaev@municorn.com>
2020-12-03 19:05:03 +0300

    add normal criteria and normalization

---
+++ ab_test_helper/normalization.py
@@ -0,0 +1,23 @@
+from scipy.stats import shapiro
+from scipy.stats import boxcox
+from statsmodels.stats.diagnostic import lilliefors
+import statsmodels.api as sm
+import pylab
+
+__author__ = 'Kirill Guliaev'
+
+def shapiro(data):
+	stat, p_value = shapiro(data)
+	return p_value
+
+def Lilliefors(data):
+	stat, p_value = lilliefors(data)
+	return p_value
+
+def show_qq_plot(data):
+	sm.qqplot(data, line='s')
+	pylab.show()
+
+def normalize_boxcox(data):
+	normalized_data, _ = boxcox(data)
+	return normalized_data
