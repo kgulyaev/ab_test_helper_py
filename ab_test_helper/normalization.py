bb118f4daf2d11a3003af96f86e1bd9823fc7608
Kirill Gulyaev <k.gulyaev@municorn.com>
2020-12-06 17:23:38 +0300

    add bootstrap of mean

--- ab_test_helper/normalization.py
+++ ab_test_helper/normalization.py
@@ -2,6 +2,9 @@ from scipy.stats import shapiro
 from scipy.stats import boxcox
 from statsmodels.stats.diagnostic import lilliefors
 import statsmodels.api as sm
+import bootstrapped.bootstrap as bs
+import bootstrapped.stats_functions as bs_stats
+import bootstrapped.compare_functions as bs_compare
 import pylab
 
 __author__ = 'Kirill Guliaev'
@@ -21,3 +24,9 @@ def show_qq_plot(data):
 def normalize_boxcox(data):
 	normalized_data, _ = boxcox(data)
 	return normalized_data
+
+def distribution_of_mean(data):
+	return bs.bootstrap(data, stat_func=bs_stats.mean, return_distribution = True)
+
+def confidence_interval_of_mean(data, alpha = 0.05):
+	return bs.bootstrap(data, stat_func=bs_stats.mean, alpha = alpha, return_distribution = False)
